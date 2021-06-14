ce6b754190a21197ab017fad41963539c0218b99
maxip1 <82190060+maxip1@users.noreply.github.com>
2021-06-14 17:06:15 -0300

    Update Dias.cpp

--- Dias.cpp
+++ Dias.cpp
@@ -27,11 +27,20 @@ int Ano (int A)  {return
     A%100 == 0 and A%400 == 0 ? 0  :
     1 ;
     };
+
+/*¿No deberia ser:
+bool Ano (unsigned A)  {return
+A%4 == 0 and (A%100 != 0 or A%400 == 0);    ?   si es verdadera va a retornar directamente 1, asi que quizas convengan cambiar la fila 45 por "Ano (A) == 1 ? 29 :"
+o creo habria que modificarla de alguna otra forma, ya que por ej el 1900 no fue bisiesto, pero es divisble por 4, ahi la formula que está entiendo que lo consideraria bisiesto.
+Podriamos meter unos assert para verificar esta formula tambien.
+La llamaria esBisiesto para que saber que es lo que se busca
+*/
+
 /*
 Se verifica cuantos dias tiene el mes
 */
-double Mes (double M, int A){ return
-    M == 1 or M==3 or M==5 or M==7 or M==8 or M==10 or M==12 ? 31:
+double Mes (double M, int A){ return                                    //¿Los tipos de datos no deberian ser unsigned?    Nombrar la funcion como getDiasDelMes
+    M == 1 or M==3 or M==5 or M==7 or M==8 or M==10 or M==12 ? 31:      //Prondiar precondiciones para los meses (que acepte de 1 a 12)
     M == 4 or M==6 or M==9 or M==11 ? 30 :
     Ano (A) == 0 ? 29 : 
     28;
